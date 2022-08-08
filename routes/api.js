e02536c6e50c7733fbcab8139d0470e67e4434e5
Anissem <dima.anissem@gmail.com>
2022-08-08 14:53:17 +0300

    add api logic

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,134 @@
-'use strict';
-
-module.exports = function (app) {
+const { ObjectId } = require('mongodb')
+module.exports = function (app, issuesCollection) {
 
   app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      let project = req.params.project;
-      
+
+    .get(async function (req, res) {
+      const { open } = req.query
+      const parsedOpen = open === 'true' ? true : open === 'false' ? false : null
+
+      const foundIssues = await issuesCollection.find({
+        ...req.query,
+        open: parsedOpen  
+      })
+
+      const response = await foundIssues.toArray()
+      console.log(response)
+      return res.send(response)
     })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
+
+    .post(async function (req, res) {
+      const {
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        status_text } = req.body
+
+      if (!issue_title || !issue_text || !created_by) {
+        return res.send({ error: 'required field(s) missing' })
+      }
+
+      const { insertedId } = await issuesCollection.insertOne({
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to: assigned_to ? assigned_to : '',
+        status_text: status_text ? status_text : '',
+        open: true,
+        created_on: new Date(),
+        updated_on: new Date()
+      }, { new: true })
+
+      const issue = await issuesCollection.findOne({ _id: insertedId })
+
+      return res.send(issue)
     })
-    
-    .put(function (req, res){
-      let project = req.params.project;
-      
+
+
+    .put(async function (req, res) {
+      const {
+        _id,
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        status_text,
+        open
+      } = req.body
+
+      try {
+
+        if (!_id) {
+          return res.send({ error: 'missing _id' })
+        }
+
+        const values = Object.entries(req.body).reduce((obj, item) => {
+          if (item[1] && item[0] !== '_id') obj[item[0]] = item[1]
+          return obj
+        }, {})
+
+        if (Object.keys(values).length <= 1) {
+          return res.send({
+            error: 'no update field(s) sent',
+            _id
+          })
+        } else {
+          const presentIssue = await issuesCollection.findOne({ _id: new ObjectId(_id) })
+
+          if (!presentIssue) {
+            return res.send({
+              error: 'could not update',
+              _id
+            })
+          }
+
+          await issuesCollection.updateOne({ _id: presentIssue._id }, {
+            $set: {
+              ...values,
+              updated_on: new Date()
+            }
+          })
+
+          return res.send({ result: 'successfully updated', _id })
+
+        }
+      } catch (error) {
+        return res.send({ error: 'could not update', _id })
+      }
     })
-    
-    .delete(function (req, res){
-      let project = req.params.project;
-      
+
+    .delete(async function (req, res) {
+      const { _id } = req.body
+      try {
+
+        if (!_id) {
+          return res.send({ error: 'missing _id' })
+        }
+
+        const presentIssue = await issuesCollection.findOne({ _id: new ObjectId(_id) })
+
+        if (!presentIssue) {
+          return res.send({ error: 'could not delete', _id })
+        }
+
+        const result = await issuesCollection.deleteOne({ _id: new ObjectId(_id) })
+
+        if (result.deletedCount === 1) {
+          return res.send({
+            result: "successfully deleted",
+            _id
+          })
+        }
+
+        return res.send({
+          error: 'could not delete',
+          _id
+        })
+      } catch (error) {
+        return res.send({ error: 'could not delete', _id })
+      }
+
     });
-    
+
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,20 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-  
+suite('Functional Tests', function () {
+    test('Create an issue with every field', (done) => {
+        chai.request(server)
+            .post('/api/issues/issuetracker')
+            .send({
+                issue_title: 'Test Issue',
+                issue_text: 'Test Issue Text',
+                created_by: 'Anissem',
+                assigned_to: 'No one',
+                status_text: 'Status'
+            }).end((err, res) => {
+                const responseKeys = ['issue_title', 'issue_text', 'created_by', 'assigned_to', 'status_text', '_id', 'created_on', 'updated_on', 'open']
+                assert.hasAllKeys(res.body, responseKeys)
+                done()
+            })
+    })
 });
